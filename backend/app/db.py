f0a7216b5590e48e722c4f3d9f4412cc4373ced1
jose valdivia <jose@localhost.localdomain>
2020-08-13 19:23:48 -0500

    delete comments

--- backend/app/db.py
+++ backend/app/db.py
@@ -7,20 +7,6 @@ from sqlalchemy.orm import sessionmaker
 
 db = SQLAlchemy()
 
-class Feedback(db.Model):
-    __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
-    rating = db.Column(db.Integer)
-    comments = db.Column(db.Text())
-
-    def __init__(self, customer, dealer, rating, comments):
-        self.customer = customer
-        self.dealer = dealer
-        self.rating = rating
-        self.comments = comments
-
 class Entity():
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime)
