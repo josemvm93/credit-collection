6724f1f3cf3f6a595915445f2d8570597cae4941
jose valdivia <jose@localhost.localdomain>
2020-08-14 10:47:31 -0500

    verify login and changes in backend

--- frontend/src/app/modules/credits/credits.service.ts
+++ frontend/src/app/modules/credits/credits.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CreditsService {
+  path = environment.apiUrl + '/login'
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  login(username: string, password: string): Observable<User> {
+    const user: User = {
+      username: username,
+      password: password,
+    }
+
+    return this.httpClient.post<User>(this.path, user)
+  }
 }
